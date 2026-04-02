08be8776ad4a6c6e5f402a11efdaddd12575f38e
kauabarbosaa1728-web <kaua.barbosaa1728@gmail.com>
2026-04-02 10:06:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from banco import criar_banco
+
 from auth import auth_bp
 from dashboard import dashboard_bp
 from estoque import estoque_bp
 from usuarios import usuarios_bp
-from pagamento import pagamento_routes  # 👈 ADICIONADO
+from pagamento import pagamento_routes
+from logs import logs_bp  # 👈 NOVO
 
 app = Flask(__name__)
 app.secret_key = "segredo123"
@@ -15,7 +17,8 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(estoque_bp)
 app.register_blueprint(usuarios_bp)
-app.register_blueprint(pagamento_routes)  # 👈 ADICIONADO
+app.register_blueprint(pagamento_routes)
+app.register_blueprint(logs_bp)  # 👈 NOVO
 
 if __name__ == "__main__":
     app.run(debug=True)
