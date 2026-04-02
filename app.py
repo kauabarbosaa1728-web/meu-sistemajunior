eafc62ef664341481172d459ba8371f4c97ac8ff
kauabarbosaa1728-web <kaua.barbosaa1728@gmail.com>
2026-04-02 13:37:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from dashboard import dashboard_bp
 from estoque import estoque_bp
 from usuarios import usuarios_bp
 from pagamento import pagamento_routes
-from logs import logs_bp  # 👈 IMPORTANTE (logs)
+from logs import logs_bp
+
+# ================= IA =================
+from ia_routes import ia_bp
 
 app = Flask(__name__)
 app.secret_key = "segredo123"
@@ -14,13 +17,17 @@ app.secret_key = "segredo123"
 # Criar banco automaticamente
 criar_banco()
 
-# Registrar rotas (blueprints)
+# ================= ROTAS =================
 app.register_blueprint(auth_bp)
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(estoque_bp)
 app.register_blueprint(usuarios_bp)
 app.register_blueprint(pagamento_routes)
-app.register_blueprint(logs_bp)  # 👈 IMPORTANTE (logs)
+app.register_blueprint(logs_bp)
 
+# ================= IA =================
+app.register_blueprint(ia_bp)
+
+# ================= START =================
 if __name__ == "__main__":
     app.run(debug=True)
