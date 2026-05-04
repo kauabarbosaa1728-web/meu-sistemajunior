0c23b7e6d8ef240663e1225211981ccba6b82a4b
kauabarbosaa1728-web <kaua.barbosaa1728@gmail.com>
2026-05-04 15:30:49 -0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-financeiro_bp = Blueprint("financeiro_bp", __name__)
-
-# IMPORTA ROTAS
-from .financeiro_main import *
-from .financeiro_entrada import *
-from .financeiro_saida import *
-from .financeiro_resumo import *
