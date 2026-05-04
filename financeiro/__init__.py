5a990c7dd782f853980006ed6fcaca3218f8a576
kauabarbosaa1728-web <kaua.barbosaa1728@gmail.com>
2026-05-04 15:46:31 -0300

    Update __init__.py

--- financeiro/__init__.py
+++ financeiro/__init__.py
@@ -2,7 +2,6 @@ from flask import Blueprint
 
 financeiro_bp = Blueprint("financeiro_bp", __name__)
 
-# IMPORTA ROTAS
 from .financeiro_main import *
 from .financeiro_entrada import *
 from .financeiro_saida import *
