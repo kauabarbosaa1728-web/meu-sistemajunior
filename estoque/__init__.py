d724468ae648b7a6bd65656107d7715cff8d9e54
kauabarbosaa1728-web <kaua.barbosaa1728@gmail.com>
2026-05-05 15:40:48 -0300

    Update __init__.py

--- estoque/__init__.py
+++ estoque/__init__.py
@@ -1,1 +1,14 @@
-from .routes import estoque_bp
+from .views_estoque import (
+    montar_alerta_estoque_baixo,
+    montar_tabela_estoque,
+    render_estoque
+)
+
+from .views_operacoes import (
+    render_editar_estoque,
+    render_produto_nao_encontrado,
+    render_erro_atualizar,
+    render_transferencia,
+    render_historico,
+    render_entrada
+)
