38eb0c060642628c5d0261d98369f71ae2c67f1c
kauabarbosaa1728-web <kaua.barbosaa1728@gmail.com>
2026-05-05 15:49:32 -0300

    Update routes.py

--- estoque/routes.py
+++ estoque/routes.py
@@ -52,10 +52,14 @@ from .service import (
     registrar_ncm
 )
 
-from .views import (
+# 🔥 CORRIGIDO AQUI
+from .views_estoque import (
     montar_alerta_estoque_baixo,
     montar_tabela_estoque,
-    render_estoque,
+    render_estoque
+)
+
+from .views_operacoes import (
     render_editar_estoque,
     render_produto_nao_encontrado,
     render_erro_atualizar,
